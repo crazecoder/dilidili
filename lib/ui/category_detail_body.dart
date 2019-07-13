a9ffd5dfa09eed61c640cf59a884d8b43b685ce7
crazecoder <21527312@qq.com>
2019-07-13 10:11:54 +0800

    修改页面布局

--- lib/ui/category_detail_body.dart
+++ lib/ui/category_detail_body.dart
@@ -1,14 +1,11 @@
-import 'dart:async';
-import 'dart:isolate';
-
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:dilidili/lib/library.dart';
 import 'package:dilidili/http.dart' as http;
-import 'package:dilidili/utils/html_util.dart';
-import 'package:dilidili/utils/my_print.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class CategoryDetailBody extends StatefulWidget {
   final String url;
@@ -22,7 +19,7 @@ class CategoryDetailBody extends StatefulWidget {
 
 class CategoryDetailBodyState extends State<CategoryDetailBody>
     with AutomaticKeepAliveClientMixin {
-  static bool _keepAlive = false;
+  static bool _keepAlive = true;
   bool isHttpComplete = false;
   var cartoons = <Cartoon>[];
   var gridItem = <Widget>[];
@@ -30,13 +27,12 @@ class CategoryDetailBodyState extends State<CategoryDetailBody>
   @override
   Widget build(BuildContext context) {
     if (isHttpComplete && widget.isShow) {
-      return new GridView.builder(
+      return new StaggeredGridView.countBuilder(
         itemCount: cartoons.length,
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        crossAxisCount: 4,
+        staggeredTileBuilder: (_index) => StaggeredTile.fit(2),
 //        mainAxisSpacing: 4.0,
 //        crossAxisSpacing: 4.0,
-        padding: const EdgeInsets.all(4.0),
 //        childAspectRatio: 1.3,
         itemBuilder: (_, i) {
           return _buildGridItem(cartoons[i]);
@@ -57,27 +53,71 @@ class CategoryDetailBodyState extends State<CategoryDetailBody>
         Application.router
             .navigateTo(context, '/detail/$url/${cartoon.name}/$picture');
       },
-      child: new Card(
-        child: new Column(
+      child: buildGridItem(cartoon),
+//      new Card(
+//        child: new Column(
+//          children: <Widget>[
+//            new Expanded(
+//              child: new CachedNetworkImage(
+//                imageUrl: cartoon.picture,
+//                placeholder: (context, str) => Center(
+//                  child: CircularProgressIndicator(),
+//                ),
+//                errorWidget: (_, _s, _o) => new Icon(Icons.error),
+//              ),
+//            ),
+//            new Text(
+//              cartoon.name,
+//              softWrap: true,
+//            ),
+//          ],
+//        ),
+//      ),
+    );
+  }
+
+  Widget buildGridItem(Cartoon cartoon) => Container(
+        width: MediaQuery.of(context).size.width / 2 - 5,
+        height: 16 * (MediaQuery.of(context).size.width / 2 - 5) / 11,
+        margin: EdgeInsets.all(5),
+//        padding: EdgeInsets.all(5),
+        child: Stack(
           children: <Widget>[
-            new Expanded(
-              child: new CachedNetworkImage(
-                imageUrl: cartoon.picture,
-                placeholder: (context,str)=>Center(
-                  child: CircularProgressIndicator(),
-                ),
-                errorWidget: (_,_s,_o)=>new Icon(Icons.error),
+            CachedNetworkImage(
+              width: MediaQuery.of(context).size.width / 2 - 5,
+              height: 16 * (MediaQuery.of(context).size.width / 2 - 5) / 11,
+              fit: BoxFit.fill,
+              imageUrl: cartoon.picture,
+              placeholder: (context, str) => Center(
+                child: CircularProgressIndicator(),
               ),
+              errorWidget: (_, _s, _o) => new Icon(Icons.error),
             ),
-            new Text(
-              cartoon.name,
-              softWrap: true,
+            Flex(
+              direction: Axis.vertical,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Opacity(
+                  child: Container(
+                    padding: EdgeInsets.all(3),
+                    width: MediaQuery.of(context).size.width / 2 - 5,
+                    child: Center(
+                      child: new Text(
+                        cartoon.name,
+                        maxLines: 1,
+//                            softWrap: true,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    color: Colors.black,
+                  ),
+                  opacity: 0.5,
+                ),
+              ],
             ),
           ],
         ),
-      ),
-    );
-  }
+      );
 
   @override
   void initState() {
